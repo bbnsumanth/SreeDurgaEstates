2fd5b0ef76ffe7743f56fb350af8a5f75ca1e876
unknown <bharat.sumanth@impetus-b0154.impetus.co.in>
2015-04-11 18:54:21 +0530

    first commit

---
+++ app/src/main/java/com/sreedurgaestates/sde/Tab3.java
@@ -0,0 +1,19 @@
+package com.sreedurgaestates.sde;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bharat.sumanth on 4/11/2015.
+ */
+public class Tab3 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_3,container,false);
+        return v;
+    }
+}
